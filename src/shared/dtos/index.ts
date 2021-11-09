a5b03774d745866f311bbebca123d1dc5205094d
jeysonlr <jeysonlr@gmail.com>
2021-11-09 16:09:41 -0300

    Alterada roda de informações para modulo  mais especifico

---
+++ src/shared/dtos/index.ts
@@ -0,0 +1,7 @@
+export * from './api-info.dto';
+export * from './response-data.dto';
+export * from './request-error.dto';
+export * from './error-response-data.dto';
+export * from './ok-response-request.dto';
+export * from './success-response-data.dto';
+export * from './created-response-data.dto';
